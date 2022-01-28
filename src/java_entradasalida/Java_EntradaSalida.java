ea95b115592784c4d08cd9a35f193d353071da29
Salvador <slb_18@hotmail.com>
2022-01-27 23:22:22 -0600

    Version1

---
+++ src/java_entradasalida/Java_EntradaSalida.java
@@ -0,0 +1,41 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_entradasalida;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Salvador
+ */
+public class Java_EntradaSalida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner  entradaDatos = new Scanner(System.in);
+        
+        //variables
+        String nombre;
+        int Edad;
+        
+        //Proceso
+        System.out.println("-----------------------------");
+        nombre = JOptionPane.showInputDialog("Digite su nombre: ");
+        Edad = Integer.parseInt(JOptionPane.showInputDialog("Digite su Edad: "));
+        
+        System.out.println("-----Sistema de Datos-----");
+        System.out.println("Hola: " + nombre + " y tienes: " + Edad + " años.");
+        System.out.println("------------------------------");
+        
+        
+        
+        
+    }
+    
+}
